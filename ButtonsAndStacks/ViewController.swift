11b3d75d07661df5d615e5cf30b071826466030b
Nipuni Obe <obe@gmail.com>
2021-02-13 19:04:48 -0500

    colors aren't working

--- ButtonsAndStacks/ViewController.swift
+++ ButtonsAndStacks/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    var colors: [UIColor] = [.red, .orange, .yellow, .blue, .systemIndigo, .systemPurple]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+   
+    @IBAction func colorButtonPressed(_ sender: UIButton) {
+        print("You clicked tag #: \(sender.tag)")
+        colorLabel.text = "You clicked \(sender.currentTitle!)"
+        colorLabel.textColor = colors[sender.tag]
+    }
 }
 
